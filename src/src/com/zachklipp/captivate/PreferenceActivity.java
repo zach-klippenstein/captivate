b0e9df88757d65e7c77b4e0020195ad6be5e9274
Zachary Klippenstein <zk.at.sf@gmail.com>
2012-04-17 22:21:02 -0500

    Added some about text, and Send Feedback button

--- src/src/com/zachklipp/captivate/PreferenceActivity.java
+++ src/src/com/zachklipp/captivate/PreferenceActivity.java
@@ -2,12 +2,20 @@ package com.zachklipp.captivate;
 
 import android.os.Bundle;
 
-public class PreferenceActivity extends android.preference.PreferenceActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        addPreferencesFromResource(R.xml.preferences);
-    }
+public class PreferenceActivity extends android.preference.PreferenceActivity
+{
+  private static final String FEEDBACK_PREFERENCE_KEY = "feedbackKey";
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    
+    addPreferencesFromResource(R.xml.preferences);
+    
+    this.getPreferenceScreen().findPreference(FEEDBACK_PREFERENCE_KEY).getIntent()
+      .putExtra(android.content.Intent.EXTRA_EMAIL,
+          new String[] {getString(R.string.feedback_email)});
+  }
 }
