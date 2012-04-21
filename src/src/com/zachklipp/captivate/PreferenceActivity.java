086a4358247e88d564a1e0832da584594c33108d
Zachary Klippenstein <zk.at.sf@gmail.com>
2012-04-21 15:54:32 -0500

    Dialog with email is shown if no feedback app available.
    Fixes #1

--- src/src/com/zachklipp/captivate/PreferenceActivity.java
+++ src/src/com/zachklipp/captivate/PreferenceActivity.java
@@ -1,10 +1,21 @@
 package com.zachklipp.captivate;
 
+import com.zachklipp.captivate.util.SafeIntentSender;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.util.Log;
 
 public class PreferenceActivity extends android.preference.PreferenceActivity
 {
   private static final String FEEDBACK_PREFERENCE_KEY = "feedbackKey";
+  private static final int NO_FEEDBACK_RECEIVER_DIALOG = 0;
+  
+  private static final String LOG_TAG = "captivate";
   
   /** Called when the activity is first created. */
   @Override
@@ -14,8 +25,55 @@ public class PreferenceActivity extends android.preference.PreferenceActivity
     
     addPreferencesFromResource(R.xml.preferences);
     
-    this.getPreferenceScreen().findPreference(FEEDBACK_PREFERENCE_KEY).getIntent()
-      .putExtra(android.content.Intent.EXTRA_EMAIL,
-          new String[] {getString(R.string.feedback_email)});
+    initializeFeedbackIntent();
+  }
+  
+  @Override
+  protected Dialog onCreateDialog(int which)
+  {
+    Dialog dialog = null;
+    
+    switch (which)
+    {
+      case NO_FEEDBACK_RECEIVER_DIALOG:
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+          .setMessage(R.string.no_feedback_receiver_message)
+          .setNeutralButton(R.string.ok, null);
+        dialog = builder.create();
+        break;
+        
+      default:
+        Log.w(LOG_TAG, String.format("Attempted to show invalid dialog: %d", which));
+    }
+    
+    return dialog;
+  }
+  
+  private void initializeFeedbackIntent()
+  {
+    Preference feedbackPreference = getPreferenceScreen().findPreference(FEEDBACK_PREFERENCE_KEY);
+    Intent primaryIntent = feedbackPreference.getIntent();
+    final SafeIntentSender sender = new SafeIntentSender(this);
+    
+    primaryIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
+        new String[] {getString(R.string.feedback_email)});
+    
+    sender.setNoReceiverHandler(new SafeIntentSender.OnNoReceiverListener()
+    {
+      @Override
+      public void onNoReceiver(Intent primary)
+      {
+        showDialog(NO_FEEDBACK_RECEIVER_DIALOG);
+      }
+    });
+    
+    feedbackPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+      @Override
+      public boolean onPreferenceClick(Preference preference)
+      {
+        sender.startActivity(preference.getIntent());
+        return true;
+      }
+    });
   }
 }
---
+++ src/src/com/zachklipp/captivate/util/SafeIntentSender.java
@@ -0,0 +1,75 @@
+package com.zachklipp.captivate.util;
+
+import java.util.List;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.util.Log;
+
+public class SafeIntentSender
+{
+  public static interface OnNoReceiverListener
+  {
+    public void onNoReceiver(Intent primary);
+  }
+  
+  private static final String LOG_TAG = "SafeIntentSender";
+  
+  private static final OnNoReceiverListener sDefaultNoReceiverListener = new OnNoReceiverListener()
+  {
+    @Override
+    public void onNoReceiver(Intent primary)
+    {
+      Log.w(LOG_TAG, String.format("No receiver for intent %s", primary.getAction()));
+    }
+  };
+  
+  private Context mContext;
+  private PackageManager mPackageManager;
+  private OnNoReceiverListener mNoReceiverHandler;
+  
+  public SafeIntentSender(Context context)
+  {
+    mContext = context;
+    mPackageManager = mContext.getPackageManager();
+  }
+  
+  public SafeIntentSender setNoReceiverHandler(OnNoReceiverListener handler)
+  {
+    mNoReceiverHandler = handler;
+    return this;
+  }
+  
+  public void startActivity(Intent primary, Intent... fallbacks)
+  {
+    List<ResolveInfo> activities = mPackageManager.queryIntentActivities(primary, 0);
+    Intent intent = primary;
+    
+    for (int i = 0; activities.size() == 0 && i < fallbacks.length; i++)
+    {
+      intent = fallbacks[i];
+      activities = mPackageManager.queryIntentActivities(primary, 0);
+    }
+    
+    if (activities.size() > 0)
+    {
+      mContext.startActivity(intent);
+    }
+    else
+    {
+      onNoReceiver(primary);
+    }
+  }
+  
+  private void onNoReceiver(Intent primary)
+  {
+    OnNoReceiverListener listener = mNoReceiverHandler;
+    
+    if (listener == null)
+      listener = sDefaultNoReceiverListener;
+    
+    listener.onNoReceiver(primary);
+  }
+}
