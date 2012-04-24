648aa20ff24f73db125b71aefd4d9b1f50cecf61
Zachary Klippenstein <zk.at.sf@gmail.com>
2012-04-24 18:44:02 -0500

    Translated ALL the things using Google Translate for #6

--- src/src/com/zachklipp/captivate/AboutActivity.java
+++ src/src/com/zachklipp/captivate/AboutActivity.java
@@ -1,8 +1,12 @@
 package com.zachklipp.captivate;
 
+import com.zachklipp.captivate.util.Log;
+
 import android.app.Activity;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity
 {
@@ -10,14 +14,34 @@ public class AboutActivity extends Activity
   @Override
   public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      
       setContentView(R.layout.about_layout);
       
-      WebView view = (WebView) findViewById(R.id.about_view);
+      setTitle(getString(R.string.about_title, getString(R.string.app_name)));
+      
+      TextView versionName = (TextView) findViewById(R.id.version_name_view);
+      versionName.setText(getString(R.string.version_name_format, getVersionName()));
+      
+      WebView view = (WebView) findViewById(R.id.about_content_view);
       
       // This is to prevent some devices from showing white artifacts at the edge of the screen.
       view.setBackgroundColor(0);
       
       view.loadUrl("file:///android_res/raw/about.html");
+  }
+  
+  private String getVersionName()
+  {
+    String versionName = "unknown";
+    
+    try
+    {
+      versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+    }
+    catch (NameNotFoundException e)
+    {
+      Log.v(e.getMessage());
+    }
+    
+    return versionName;
   }
 }
--- src/src/com/zachklipp/captivate/PreferenceActivity.java
+++ src/src/com/zachklipp/captivate/PreferenceActivity.java
@@ -7,12 +7,16 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 
 public class PreferenceActivity extends android.preference.PreferenceActivity
 {
-  private static final String FEEDBACK_PREFERENCE_KEY = "feedbackKey";
+  private static final String ENABLED_PREFERENCE_KEY = "enabled_pref";
+  private static final String FEEDBACK_PREFERENCE_KEY = "feedback_pref";
+  private static final String ABOUT_PREFERENCE_KEY = "about_pref";
+  
   private static final int NO_FEEDBACK_RECEIVER_DIALOG = 0;
   
   /** Called when the activity is first created. */
@@ -22,6 +26,8 @@ public class PreferenceActivity extends android.preference.PreferenceActivity
     super.onCreate(savedInstanceState);
     
     addPreferencesFromResource(R.xml.preferences);
+    
+    formatStrings();
     
     initializeFeedbackIntent();
   }
@@ -35,7 +41,8 @@ public class PreferenceActivity extends android.preference.PreferenceActivity
     {
       case NO_FEEDBACK_RECEIVER_DIALOG:
         AlertDialog.Builder builder = new AlertDialog.Builder(this)
-          .setMessage(R.string.no_feedback_receiver_message)
+          .setMessage(getString(
+              R.string.no_feedback_receiver_message, getString(R.string.feedback_email)))
           .setNeutralButton(R.string.ok, null);
         dialog = builder.create();
         break;
@@ -47,11 +54,27 @@ public class PreferenceActivity extends android.preference.PreferenceActivity
     return dialog;
   }
   
+  private void formatStrings()
+  {
+    formatPreferenceTitle(FEEDBACK_PREFERENCE_KEY, R.string.feedback_email);
+    formatPreferenceTitle(ABOUT_PREFERENCE_KEY, R.string.app_name);
+    
+    CheckBoxPreference enabledPref = (CheckBoxPreference)
+        getPreferenceManager().findPreference(ENABLED_PREFERENCE_KEY);
+    enabledPref.setSummaryOn(
+        formatCharSequence(enabledPref.getSummaryOn(), R.string.app_name));
+    enabledPref.setSummaryOff(
+        formatCharSequence(enabledPref.getSummaryOff(), R.string.app_name));
+  }
+  
   private void initializeFeedbackIntent()
   {
     Preference feedbackPreference = getPreferenceScreen().findPreference(FEEDBACK_PREFERENCE_KEY);
     Intent primaryIntent = feedbackPreference.getIntent();
     final SafeIntentSender sender = new SafeIntentSender(this);
+    
+    primaryIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+        getString(R.string.feedback_subject, getString(R.string.app_name)));
     
     primaryIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
         new String[] {getString(R.string.feedback_email)});
@@ -73,5 +96,23 @@ public class PreferenceActivity extends android.preference.PreferenceActivity
         return true;
       }
     });
+  }
+  
+  private void formatPreferenceTitle(CharSequence key, int... args)
+  {
+    Preference pref = getPreferenceManager().findPreference(key);
+    pref.setTitle(formatCharSequence(pref.getTitle(), args));
+  }
+  
+  private CharSequence formatCharSequence(CharSequence format, int... args)
+  {
+    Object[] strArgs = new Object[args.length];
+    
+    for (int i = 0; i < args.length; i++)
+    {
+      strArgs[i] = getString(args[i]);
+    }
+    
+    return String.format(format.toString(), strArgs);
   }
 }
