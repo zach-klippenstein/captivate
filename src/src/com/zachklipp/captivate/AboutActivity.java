b7ce91c97ab6648a4d355a517d58a1042a89d1d9
Zachary Klippenstein <zk.at.sf@gmail.com>
2012-04-24 17:14:14 -0500

    Moved About style to own file, and hard-coded version number.
    This makes the code a lot simpler (and faster), at the (relatively
    inexpensive) cost of having to remember to update the version number
    in two places.
    
    This is in preparation for localizing the about page (see #6)

--- src/src/com/zachklipp/captivate/AboutActivity.java
+++ src/src/com/zachklipp/captivate/AboutActivity.java
@@ -1,12 +1,6 @@
 package com.zachklipp.captivate;
 
-import java.nio.charset.Charset;
-
-import com.zachklipp.captivate.util.HtmlResourceTemplate;
-import com.zachklipp.captivate.util.Log;
-
 import android.app.Activity;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.webkit.WebView;
 
@@ -24,34 +18,6 @@ public class AboutActivity extends Activity
       // This is to prevent some devices from showing white artifacts at the edge of the screen.
       view.setBackgroundColor(0);
       
-      // Must use this variation of load() to get image to load.
-      view.loadDataWithBaseURL("file:///android_res",
-          getAboutHtml(),
-          "text/html",
-          Charset.defaultCharset().name(),
-          null);
-  }
-  
-  private String getAboutHtml()
-  {
-    return new HtmlResourceTemplate()
-      .withValues("version-name", getVersionName())
-      .render(this, R.raw.about);
-  }
-  
-  private String getVersionName()
-  {
-    String versionName = "unknown";
-    
-    try
-    {
-      versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-    }
-    catch (NameNotFoundException e)
-    {
-      Log.v(e.getMessage());
-    }
-    
-    return versionName;
+      view.loadUrl("file:///android_res/raw/about.html");
   }
 }
